f0b4babd33cfee9b18a7f028658661cbc3c61dac
Thierry PHILIPPE <thierryphilippe@Macbook-Pro-de-Thierry.local>
2017-07-17 23:50:11 +0200

    Added build task to make things better.

---
+++ index.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var AB = require('another-brick');
+
+var Plugin = function(opt) {
+  this.settings = AB.extend(true, Plugin.defaults, opt);
+  this.queries  = this.settings.bp;
+  this.current  = [];
+  this.animated = false;
+
+  this.init();
+};
+
+Plugin.defaults = {
+  bp: {}
+};
+
+Plugin.prototype = {
+  init: function() {
+    this.current = this._getCurrent();
+    this._watcher();
+
+    return this;
+  },
+
+  _getCurrent: function() {
+    var sizes = [];
+
+    for (var key in this.queries) {
+      if (!this.queries.hasOwnProperty(key))
+        continue;
+
+      if (window.matchMedia(this.queries[key]).matches)
+        sizes.push(key);
+    }
+
+    return sizes;
+  },
+
+  _watcher: function() {
+    var that  = this,
+        event = new CustomEvent('changed.ab-mediaquery'),
+        newSize, resizeTimer;
+
+    window.onresize = function() {
+      if (!that.animated) {
+        window.requestAnimationFrame(that._updateSizes.bind(that));
+        that.animated = true;
+      }
+    };
+  },
+
+  _updateSizes: function() {
+    var newSize = this._getCurrent(),
+        event   = new CustomEvent('changed.ab-mediaquery');
+
+    this.animated = false;
+
+    // check if it's updated
+    if (newSize.join('|') !== this.current.join('|')) {
+      this.current = newSize;
+      window.dispatchEvent(event);
+    }
+  },
+
+  is: function(size) {
+    return window.matchMedia(this.queries[size]).matches;
+  }
+};
+
+var abMediaQuery = function(opt) {
+  AB.mediaQuery = new Plugin(opt);
+};
+
+module.exports = abMediaQuery;
